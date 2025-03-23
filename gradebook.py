5e3d2b23922fbabebd96e8ad4e929d1650240e0f
isiah-a <isiaha478@gmail.com>
2025-03-23 14:33:19 -0400

    added instructor class and multiple student classes

--- gradebook.py
+++ gradebook.py
@@ -1,4 +1,5 @@
 import enum
+import uuid
 
 
 class AliveStatus(enum.Enum):
@@ -23,5 +24,31 @@ class Person:
 
     def update_status(self, status):
         self.alive = status
+
+class Instructor(Person):
+    def __init__(self, first_name, last_name, dob, AliveStatus):
+        super().__init__(first_name, last_name, dob, AliveStatus)
+        self.instructor_id = ("Instructor_" + str(uuid.uuid4()))
+
+class Student(Person):
+    def __init__(self, first_name, last_name, dob, AliveStatus):
+        super().__init__(first_name, last_name, dob, AliveStatus)
+        self.student_id = ("Student_" + str(uuid.uuid4()))
+
+class ZipCodeStudent(Student):
+    def __init__(self, first_name, last_name, dob, AliveStatus):
+        super().__init__(first_name, last_name, dob, AliveStatus)
+
+class PerK(Student):
+    def __init__(self, first_name, last_name, dob, AliveStatus):
+        super().__init__(first_name, last_name, dob, AliveStatus)
+
+class MiddleSchool(Student):
+    def __init__(self, first_name, last_name, dob, AliveStatus):
+        super().__init__(first_name, last_name, dob, AliveStatus)
+
+class CollegeStudent(Student):
+    def __init__(self, first_name, last_name, dob, AliveStatus):
+        super().__init__(first_name, last_name, dob, AliveStatus)
 
 
